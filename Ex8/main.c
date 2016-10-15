ad86bbade4b40ca4537d77b884025deb45a56f6f
gmocellin <giovane.mocellin>
2016-10-15 16:47:38 -0300

    Exercicio 8 completo

--- Ex8/main.c
+++ Ex8/main.c
@@ -3,41 +3,86 @@
 #include <string.h>
 #include <math.h>
 
+void print_game(char game[5][5]){
+    int i, j;
+    for(i = 0; i < 5; i++ ){
+        for(j = 0; j < 4; j++){
+            printf("%c ", game[i][j]);
+        }
+        printf("%c\n", game[i][j]);
+    }
+}
+
 int main(){
 
-    float total_bytes_left, sum_bytes;
-    int count_total;
-    char read_aux[100];
+    char game[5][5], read_aux;
+    char move;
 
-    scanf("%f", &total_bytes_left);
+    int i, j, blank[2], fail;
 
-    printf("Data size of %.0f bytes:\n", total_bytes_left);
+    blank[0] = 0;
+    blank[1] = 0;
 
-    sum_bytes = 0;
-    count_total = 0;
-
-    scanf("%s", read_aux);
-    while(strcmp(read_aux, "F\0") != 0){
-        sum_bytes += atoi(read_aux);
-
-        count_total++;
-
-        if(count_total%5 == 0){
-
-            total_bytes_left -= sum_bytes;
-
-            if(sum_bytes == 0){
-                printf("Time remaining: stalled\n");
-            } else {
-                printf("Time remaining: %.0lf seconds\n", ceil(total_bytes_left/(sum_bytes/5))); 
+    for(i = 0; i < 5; i++ ){
+        scanf("%c", &read_aux);
+        for(j = 0; read_aux != '\n'; j++){
+            game[i][j] = read_aux;
+            if(read_aux == ' '){
+                blank[0] = i;
+                blank[1] = j;
             }
-            sum_bytes = 0;
+            scanf("%c", &read_aux);
         }
-
-        scanf("%s", read_aux);
     }
 
-    printf("Total time: %d seconds", count_total);
+    fail = 0;
+    scanf("%c", &move);
+    while(move != 'O'){
+        //printf("%c\t", move);
+        //printf("%d %d\n", blank[0], blank[1]);
+
+        if(move == 'B'){
+            if(blank[0] == 4){
+                fail = 1;
+                break;
+            }
+            game[blank[0]][blank[1]] = game[blank[0]+1][blank[1]];
+            game[blank[0]+1][blank[1]] = ' ';
+            blank[0]++;
+        } else if(move == 'A'){
+            if(blank[0] == 0){
+                fail = 1;
+                break;
+            }
+            game[blank[0]][blank[1]] = game[blank[0]-1][blank[1]];
+            game[blank[0]-1][blank[1]] = ' ';
+            blank[0]--;
+        } else if(move == 'R'){
+            if(blank[1] == 4){
+                fail = 1;
+                break;
+            }
+            game[blank[0]][blank[1]] = game[blank[0]][blank[1]+1];
+            game[blank[0]][blank[1]+1] = ' ';
+            blank[1]++;
+        } else if(move == 'L'){
+            if(blank[1] == 0){
+                fail = 1;
+                break;
+            }
+            game[blank[0]][blank[1]] = game[blank[0]][blank[1]-1];
+            game[blank[0]][blank[1]-1] = ' ';
+            blank[1]--;
+        }
+
+        scanf("%c", &move);
+    }
+
+    if(fail){
+        printf("This puzzle has no final configuration.");
+    }else{
+        print_game(game);
+    }
 
     return 0;
 }
